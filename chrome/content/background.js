3f95231d16c2eb5e570d22a469450e4a8544bba3
Jonathan Kamens <jik@kamens.us>
2023-08-12 09:08:50 -0400

    Delete a couple of obsolete files no longer needed in TB102/115
    These files got missed in the initial conversion to TB115 because of a
    merge error.

--- chrome/content/background.js
+++
@@ -1,8 +0,0 @@
-var onload=function() {
-   browser.toggl_bgrndAPI.OnLoad()
-}
-var OnLoadObserve=function(){
-   browser.toggl_bgrndAPI.onloadObserver();
-}
-OnLoadObserv();
-browser.runtime.onInstalled.addListener(onload);
